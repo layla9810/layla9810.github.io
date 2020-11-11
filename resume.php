e58f20a33fec42b765c3e771b9589148138ed820
Layla Santana <laylasmota@gmail.com>
2020-11-11 20:28:17 -0300

    First

---
+++ resume.php
@@ -0,0 +1,313 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Layla Santana Mota</title>
+
+    <!-- Meta Data -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <meta name="author" content="Layla Santana Mota" />
+    <meta name="description" content="Portifólio de trabalhos realizados e formação de Layla Santana Mota" />
+    <!-- Styles -->
+    <link rel="stylesheet" type="text/css" href="assets/styles/style.css" />
+    <link rel="stylesheet" type="text/css" href="assets/styles/style-dark.css" />
+</head>
+
+<body class="bg-triangles">
+    <!-- Preloader -->
+    <div class="preloader">
+        <div class="preloader__wrap">
+            <div class="circle-pulse">
+                <div class="circle-pulse__1"></div>
+                <div class="circle-pulse__2"></div>
+            </div>
+            <div class="preloader__progress"><span></span></div>
+        </div>
+    </div>
+
+    <main class="main">
+        <div class="container gutter-top">
+            <!-- Header -->
+            <header class="header box">
+                <div class="header__left">
+                    <div class="header__photo">
+                        <img class="header__photo-img" src="assets/img/layla.jpeg" alt="Layla Santana Mota">
+                    </div>
+                    <div class="header__base-info">
+                        <h4 class="title titl--h4">Layla Santana Mota</h4>
+                        <div class="status">Empreendedora</div>
+                        <ul class="header__social">
+                            <li>
+                                <a href="https://www.linkedin.com/in/layla-santana-0787a2198//">
+                                    <i class="font-icon icon-linkedin"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://github.com/layla9810">
+                                    <i class="font-icon icon-github"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.youtube.com/feed/my_videos">
+                                    <i class="font-icon icon-youtube"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://twitter.com/LaylaSMota">
+                                    <i class="font-icon icon-twitter"></i>
+                                </a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+                <div class="header__right">
+                    <ul class="header__contact">
+                        <li>
+                            <span class="overhead">
+                                Email
+                            </span>
+                            layla@santana.com
+                        </li>
+                        <li>
+                            <span class="overhead">Telefone</span><a
+                                href="https://api.whatsapp.com/send?phone=5563992999999">+55 (63) 99299-9999 </a>
+                        </li>
+                    </ul>
+                    <ul class="header__contact">
+
+                        <li>
+                            <span class="overhead">
+                                Endereço
+                            </span>
+                            Endereço Comercial - UF
+                        </li>
+                    </ul>
+                </div>
+            </header>
+
+            <div class="row sticky-parent">
+                <!-- Sidebar nav -->
+                <aside class="col-12 col-md-12 col-lg-2">
+                    <div class="sidebar box sticky-column">
+                        <ul class="nav">
+                            <li class="nav__item"><a href="index.php"><i class="icon-user"></i>Sobre Mim</a></li>
+                            <li class="nav__item"><a class="active" href="resume.php"><i
+                                        class="icon-file-text"></i>Currículo</a></li>
+                            <li class="nav__item"><a href="works.php"><i class="icon-codesandbox"></i>Portifólio</a>
+                            </li>
+                        </ul>
+                    </div>
+                </aside>
+
+                <!-- Conteudo -->
+                <div class="col-12 col-md-12 col-lg-10">
+                    <div class="box box-content">
+                        <div class="pb-2">
+                            <h1 class="title title--h1 first-title title__separate">Currículo</h1>
+                        </div>
+
+                        <!-- Experience -->
+                        <div class="row">
+                            <div class="col-12">
+                                <h2 class="title title--h3"><img class="title-icon"
+                                        src="assets/icons/dark/icon-education.svg" alt="" /> Educação</h2>
+                                <div class="timeline">
+                                    <!-- Item -->
+                                    <article class="timeline__item">
+                                        <h5 class="title title--h5 timeline__title">Informática Básica.</h5>
+                                        <span class="timeline__period">2010 — 2010</span>
+                                        <p class="timeline__description">
+                                        ProSoft Informática</p>
+                                    </article>
+
+                                    <!-- Item -->
+                                    <article class="timeline__item">
+                                        <h5 class="title title--h5 timeline__title">Licenciatura em Língua Portuguesa.
+                                        </h5>
+                                        <span class="timeline__period">2014 — 2018</span>
+                                        <p class="timeline__description">Universidade Federal do Tocantins.</p>
+                                    </article>
+
+                                    <!-- Item -->
+                                    <article class="timeline__item">
+                                        <h5 class="title title--h5 timeline__title">
+                                        Seminário Internacional - Educação em ambiente prisional: Políticas e práticas no Brasil e em Portugal.
+                                        </h5>
+                                        <span class="timeline__period">2020 — 2020</span>
+                                        <p class="timeline__description">Universidade Federal de Pernambuco.</p>
+                                    </article>
+                                    <!-- Item -->
+                                    <article class="timeline__item">
+                                        <h5 class="title title--h5 timeline__title">Cursos Extracurriculares</h5>
+                                        <span class="timeline__period">2014 — 2018</span>
+                                        <p class="timeline__description">Universidade Federal do Tocantins</p>
+                                        <ul>
+                                            <li>I Seminário do Laboratório de Línguas Indígenas e do Núcleo </li>
+                                            <li>I Seminário de Literatura Tocantinense. </li>
+                                            <li>V Primavera Sem Flores</li>
+                                        </ul>
+                                    </article>
+                                    <article class="timeline__item">
+
+                                        
+
+
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <h2 class="title title--h3"><img class="title-icon"
+                                        src="assets/icons/dark/certificado.png" alt="" /> Certificação</h2>
+                                <div class="timeline">
+                                    <!-- Item -->
+                                    <article class="timeline__item">
+                                        <h5 class="title title--h5 timeline__title">Certificação 1</h5>
+                                        <span class="timeline__period">2018</span>
+                                        <p class="timeline__description">Universidade Federal do Tocantins</p>
+                                    </article>
+                                    <!-- Item -->
+                                    <article class="timeline__item">
+                                        <h5 class="title title--h5 timeline__title">Certificação 1</h5>
+                                        <span class="timeline__period">2020</span>
+                                        <p class="timeline__description">Sebrae</p>
+                                    </article>
+                                    <!-- Item -->
+                                    <article class="timeline__item">
+                                        <h5 class="title title--h5 timeline__title">Certificação 1</h5>
+                                        <span class="timeline__period">2020</span>
+                                        <p class="timeline__description">Universidade Federal de Pernambuco</p>
+                                    </article>
+
+                                </div>
+                            </div>
+
+                            <!--Experiencia-->
+
+                            <div class="col-12">
+                                <h2 class="title title--h3"><img class="title-icon"
+                                        src="assets/icons/dark/icon-experience.svg" alt="" /> Experiencia</h2>
+                                <div class="timeline">
+                                    <!-- Item -->
+                                    <article class="timeline__item">
+                                        <h5 class="title title--h5 timeline__title">Professora de Língua Portuguesa</h5>
+                                        <span class="timeline__period">2019 — 2019</span>
+                                        <p class="timeline__description">Possui atuação profissional como Monitora de Língua Portuguesa no Programa “Mais Educação.</p>
+                                    </article>
+                                    <!-- Item -->
+                                    <article class="timeline__item">
+                                        <h5 class="title title--h5 timeline__title">Secretária de advocacia</h5>
+                                        <span class="timeline__period">2020 — 2020</span>
+                                        <p class="timeline__description">Atendimento de telefonemas, a transmissão de recados, a organização da agenda e até alguns cuidados relativos à vida pessoal do advogado.</p>
+                                    </article>
+                                    <!-- Item -->
+                                    <article class="timeline__item">
+                                        <h5 class="title title--h5 timeline__title">Vendedora</h5>
+                                        <span class="timeline__period">2015 — 2015</span>
+                                        <p class="timeline__description">Responsável por realizar vendas e reposição de estoque. </p>
+                                    </article>
+                                    <!-- Item -->
+                                    
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Skills -->
+                        <div class="row">
+                            <div class="col-12">
+                                <h2 class="title title--h3">Habilidades</h2>
+                                <div class="box box__border">
+                                    <!-- Progress -->
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="50"
+                                            aria-valuemin="0" aria-valuemax="100">
+                                            <div class="progress-text"><span>INGLÊS</span><span>60%</span></div>
+                                        </div>
+                                        <div class="progress-text"><span>INGLÊS</span></div>
+                                    </div>
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="60"
+                                            aria-valuemin="0" aria-valuemax="100">
+                                            <div class="progress-text"><span>LEITURA</span><span>100%</span></div>
+                                        </div>
+                                        <div class="progress-text"><span>LEITURA</span></div>
+                                    </div>
+
+                                    <!-- Progress -->
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="40"
+                                            aria-valuemin="0" aria-valuemax="100">
+                                            <div class="progress-text"><span>INTERPRETAÇÃO</span><span>100%</span></div>
+                                        </div>
+                                        <div class="progress-text"><span>INTERPRETAÇÃO</span></div>
+                                    </div>
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="40"
+                                            aria-valuemin="0" aria-valuemax="100">
+                                            <div class="progress-text"><span>ESCRITA</span><span>100%</span></div>
+                                        </div>
+                                        <div class="progress-text"><span>ESCRITA</span></div>
+                                    </div>
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="40"
+                                            aria-valuemin="0" aria-valuemax="100">
+                                            <div class="progress-text"><span>GRAMÁTICA</span><span>100%</span></div>
+                                        </div>
+                                        <div class="progress-text"><span>GRAMÁTICA</span></div>
+                                    </div>
+
+                                    <!-- Progress -->
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="50"
+                                            aria-valuemin="0" aria-valuemax="100">
+                                            <div class="progress-text"><span>SINTAXE</span><span>90%</span></div>
+                                        </div>
+                                        <div class="progress-text"><span>SINTAXE</span></div>
+                                    </div>
+
+                                    <!-- Progress -->
+                                    <div class="progress">
+                                        <div class="progress-bar" role="progressbar" aria-valuenow="70"
+                                            aria-valuemin="0" aria-valuemax="100">
+                                            <div class="progress-text"><span>SEMÂNTICA</span><span>100%</span></div>
+                                        </div>
+                                        <div class="progress-text"><span>SEMÂNTICA</span></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Footer -->
+                    <footer class="footer">© Layla Santana Mota</footer>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <!-- SVG masks -->
+    <svg class="svg-defs">
+        <clipPath id="avatar-box">
+            <path
+                d="M1.85379 38.4859C2.9221 18.6653 18.6653 2.92275 38.4858 1.85453 56.0986.905299 77.2792 0 94 0c16.721 0 37.901.905299 55.514 1.85453 19.821 1.06822 35.564 16.81077 36.632 36.63137C187.095 56.0922 188 77.267 188 94c0 16.733-.905 37.908-1.854 55.514-1.068 19.821-16.811 35.563-36.632 36.631C131.901 187.095 110.721 188 94 188c-16.7208 0-37.9014-.905-55.5142-1.855-19.8205-1.068-35.5637-16.81-36.63201-36.631C.904831 131.908 0 110.733 0 94c0-16.733.904831-37.9078 1.85379-55.5141z" />
+        </clipPath>
+        <clipPath id="avatar-hexagon">
+            <path
+                d="M0 27.2891c0-4.6662 2.4889-8.976 6.52491-11.2986L31.308 1.72845c3.98-2.290382 8.8697-2.305446 12.8637-.03963l25.234 14.31558C73.4807 18.3162 76 22.6478 76 27.3426V56.684c0 4.6805-2.5041 9.0013-6.5597 11.3186L44.4317 82.2915c-3.9869 2.278-8.8765 2.278-12.8634 0L6.55974 68.0026C2.50414 65.6853 0 61.3645 0 56.684V27.2891z" />
+        </clipPath>
+    </svg>
+
+    <div class="back-to-top"></div>
+
+    <!-- JavaScripts -->
+    <script src="assets/js/jquery-3.4.1.min.js"></script>
+    <script src="assets/js/plugins.min.js"></script>
+    <script src="assets/js/common.js"></script>
+
+
+</body>
+
+</html>
